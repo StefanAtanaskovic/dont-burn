bab93f794051cb0a2eeeaa2cff5b6f41b3c506e0
StefanAtanaskovic <35070029+StefanAtanaskovic@users.noreply.github.com>
2026-02-26 18:51:45 +0100

    Add ability mechanics for ball, punch, and blink

---
+++ js/abilities.js
@@ -0,0 +1,19 @@
+// Abilities Mechanics
+
+// Ball ability
+function ball() {
+    // Implement ball ability mechanics here
+    console.log('Ball ability activated!');
+}
+
+// Punch ability
+function punch() {
+    // Implement punch ability mechanics here
+    console.log('Punch ability activated!');
+}
+
+// Blink ability
+function blink() {
+    // Implement blink ability mechanics here
+    console.log('Blink ability activated!');
+}
